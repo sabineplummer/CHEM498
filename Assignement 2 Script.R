2e71fc96d4f22667789e7861e6d7f7b5a300d0ec
Sabine Plummer <sabine.plummer@mail.concordia.ca>
2020-10-06 19:09:05 -0400

    minimal filters work, you got a semi-clean set!
    SP 06/10/2020

--- Assignement 2 Script.R
+++ Assignement 2 Script.R
@@ -11,67 +11,95 @@ library(dplyr)
 library(naniar)
 
 
-# Importing Data from CSV files
+# STEP 1 --> see documentation
+
+
+# STEP 2 --> Import Data into R
 
 # Data set 1
-ENclimate11 <- read.csv("dirty_en_climate_hourly_QC_702S006_11-2019_P1H.csv", stringsAsFactors = FALSE) #importing Environment Canada data
-str(ENclimate11) #look at column data types post import
+ENclimate11 <- read.csv("dirty_en_climate_hourly_QC_702S006_11-2019_P1H.csv", stringsAsFactors = FALSE) # importing Environment Canada data
+str(ENclimate11) # look at column data types post import
 
 # Data set 2
-ENclimate12 <- read.csv("dirty_en_climate_hourly_QC_702S006_12-2019_P1H.csv", stringsAsFactors = FALSE) #importing Environment Canada data
-str(ENclimate12) #look at column data types post import
+ENclimate12 <- read.csv("dirty_en_climate_hourly_QC_702S006_12-2019_P1H.csv", stringsAsFactors = FALSE) # importing Environment Canada data
+str(ENclimate12) # look at column data types post import
 
 # Data set 3
-RSQA <- read.csv("dirty_RSQA,  station99, 1 nov 2019-13 apr 2020.csv", stringsAsFactors = FALSE) #importing RSQA data
-str(RSQA) #look at column data types post import
+RSQA <- read.csv("dirty_RSQA,  station99, 1 nov 2019-13 apr 2020.csv", stringsAsFactors = FALSE) # importing RSQA data
+str(RSQA) # look at column data types post import
 
 
-# Arranging data types
+# STEP 3 --> Find and Treat Erroneous Data (Arranging data types)
 
 # ENclimate11
 
-ENclimate11$Date.Time = as.POSIXct(ENclimate11$Date.Time)
-ENclimate11$Temp...C. = as.numeric(ENclimate11$Temp...C.)
-ENclimate11$Dew.Point.Temp...C. = as.numeric(ENclimate11$Dew.Point.Temp...C.)
+ENclimate11$Date.Time = as.POSIXct(ENclimate11$Date.Time) # making all date equate
+ENclimate11$Temp...C. = as.numeric(ENclimate11$Temp...C.) # as numeric to allow merging + NA any non numeric observations
+ENclimate11$Dew.Point.Temp...C. = as.numeric(ENclimate11$Dew.Point.Temp...C.) # ^^^
 str(ENclimate11)
 
 # ENclimate12
 
-ENclimate12$Date.Time = as.POSIXct(ENclimate12$Date.Time)
-ENclimate12$Temp...C. = as.numeric(ENclimate12$Temp...C.)
-ENclimate12$Dew.Point.Temp...C. = as.numeric(ENclimate12$Dew.Point.Temp...C.)
-ENclimate12$Stn.Press..kPa. = as.numeric(ENclimate12$Stn.Press..kPa.)
+ENclimate12$Date.Time = as.POSIXct(ENclimate12$Date.Time) # making all date equate
+ENclimate12$Temp...C. = as.numeric(ENclimate12$Temp...C.) # as numeric to allow merging + NA any non numeric observations
+ENclimate12$Dew.Point.Temp...C. = as.numeric(ENclimate12$Dew.Point.Temp...C.) # ^^^
+ENclimate12$Stn.Press..kPa. = as.numeric(ENclimate12$Stn.Press..kPa.) # ^^^
 str(ENclimate12)
 
 # RSQA
 
-RSQA$Date...Time = as.POSIXct(RSQA$Date...Time, format = "%d-%m-%Y %H:%M")
-RSQA$PM2.5..ug.m3. = as.numeric(RSQA$PM2.5..ug.m3.)
-RSQA$PM10..ug.m3. = as.numeric(RSQA$PM10..ug.m3.)
-RSQA$O3..ppb. = as.numeric(RSQA$O3..ppb.)
-RSQA$NO..ppb. = as.numeric(RSQA$NO..ppb.)
-RSQA$NO2..ppb. = as.numeric(RSQA$NO2..ppb.)
-RSQA$SO2..ppb. = as.numeric(RSQA$SO2..ppb.)
+RSQA$Date...Time = as.POSIXct(RSQA$Date...Time, format = "%d-%m-%Y %H:%M") # making all date equate, had to format to compensate for different date style
+RSQA$PM2.5..ug.m3. = as.numeric(RSQA$PM2.5..ug.m3.) # as numeric to allow merging + NA any non numeric observations
+RSQA$PM10..ug.m3. = as.numeric(RSQA$PM10..ug.m3.) # ^^^
+RSQA$O3..ppb. = as.numeric(RSQA$O3..ppb.) # ^^^
+RSQA$NO..ppb. = as.numeric(RSQA$NO..ppb.) # ^^^
+RSQA$NO2..ppb. = as.numeric(RSQA$NO2..ppb.) # ^^^
+RSQA$SO2..ppb. = as.numeric(RSQA$SO2..ppb.) # ^^^
 str(RSQA)
 
 
-# Merging data sets
+# STEP 4 --> Merging data sets
 
-merged_data1 <- rbind(ENclimate11, ENclimate12)
+merged_data1 <- rbind(ENclimate11, ENclimate12) # merging similar dataframes by appending one to the other
 str(merged_data1)
 
-merged_data2 <- merge(merged_data1, RSQA, by.x = "Date.Time", by.y = "Date...Time", all.x = FALSE, all.y = TRUE)
+merged_data2 <- merge(merged_data1, RSQA, by.x = "Date.Time", by.y = "Date...Time", all.x = FALSE, all.y = TRUE) # merging dataframes by date
 str(merged_data2)
 
 
+# STEP 5 --> see documentation
+
+
+# STEP 6 --> Cleaning Data
+
 # Making vector of missing data variables
 
-M <- c("NoData", 999, "999", -999, "-999", -1999, "-1999", "", " ", "<Samp", "InVld", "Down", "Calib", "Zero") 
+M <- c("NoData", 999, "999", -999, "-999", -1999, "-1999", "", " ", "<Samp", "InVld", "Down", "Calib", "Zero") # 
 
 # Replacing all missing values with NA
 
 merged_data2_NA <- merged_data2 %>% replace_with_na_all(condition = ~.x %in% M) #replaces any missing data term in the dataframe with NA
 str(merged_data2_NA)
 
+# filters
+
+merged_data_clean <- merged_data2_NA %>% filter (
+  PM2.5..ug.m3. >= 0, 
+  PM2.5..ug.m3. < 200,
+  PM10..ug.m3.  >= 0, 
+  PM10..ug.m3.  < 200,
+  O3..ppb. >= 0, 
+  O3..ppb. < 150,
+  NO..ppb. >= 0, 
+  NO..ppb. < 100,
+  NO2..ppb. >= 0, 
+  NO2..ppb. < 100,
+  SO2..ppb. >= 0,
+  SO2..ppb. < 20
+)
+
+
+
+    
 
 
